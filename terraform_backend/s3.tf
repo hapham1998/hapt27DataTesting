00cf4b05fd6e162879a4429b7e8b0b0b7cbe351a
hacute <phamha.hyp@gmail.com>
2024-03-17 14:05:22 +0700

    Update account name

--- terraform_backend/s3.tf
+++ terraform_backend/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "qa-community-hapr27-backend-s3"
+  bucket = "qa-community-hapt27-backend-s3"
   lifecycle {
     prevent_destroy = true
   }
